178717e24640ffffff89370a4cd1ad930226d0d5
gottagitgoing <ruo.cxie@gmail.com>
2019-08-28 12:51:23 -0400

    Update Bagging.py

--- Tree-Based Algorithms/Bagging.py
+++ Tree-Based Algorithms/Bagging.py
@@ -1,3 +1,5 @@
+# This code is created and modified by Andy Xie and Tomas Tokar at Krembil in 2018.
+
 import numpy as np
 import pandas as pd
 from sklearn import preprocessing
@@ -7,7 +9,7 @@ from sklearn.ensemble import BaggingRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_predict
 import matplotlib.pyplot as plt
-'''
+
 d1 = pd.DataFrame(pd.read_csv('C:\Users\Andy Xie\Documents\Work\Research\Databases\MI'+'\\'+'final_db_standardized.txt', sep = '\t', low_memory = False))
 d2 = pd.DataFrame(pd.read_csv('C:\Users\Andy Xie\Documents\Work\Research\Databases\MI'+'\\'+'training_validated_interactions.txt', sep = '\t', low_memory = False))
 
@@ -31,7 +33,7 @@ y = ml_file.Validated
 binr = preprocessing.LabelBinarizer()
 binr.fit(y)
 y = binr.transform(y)
-'''
+
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.3, random_state = 1)
 
 tree = DecisionTreeRegressor(max_depth = 800, criterion = 'friedman_mse', min_samples_leaf = 15, random_state = 1)
@@ -40,12 +42,6 @@ m = bag.fit(X_train,y_train)
 pred = cross_val_predict(m,X_test,y_test,cv = 10) # prb
 pt = pd.DataFrame({'TF_Target' : X_test.index, 'proba' : pred, 'truth' : y_test[:,0]})
 pt = pt.sort_values(by = 'proba', ascending = False) #//lm = linear_model.LogisticRegression()
-
-lm = linear_model.LogisticRegression()
-lm.fit(X_train, y_train)
-p = lm.predict_proba(X_test)
-ptt = pd.DataFrame({'TF_Target' : X_test.index, 'proba' : p[:,1], 'truth' : y_test[:,0]})
-ptt = ptt.sort_values(by = 'proba', ascending = False)
 
 # F1 Score Analysis
 idx = np.linspace(1, np.log10(pt.shape[0]), 20) #//
@@ -62,41 +58,10 @@ for i in idx:
     # Add to list
     f1_score.append(f1)
 
-idxx = np.linspace(1, np.log10(ptt.shape[0]), 20)
-f1_score2 = list()
-for i in idxx:
-    sptt = ptt.head(np.int(10**i))
-    fnn = np.sum(~d2['Factor / Gene Name'].isin(sptt.TF_Target))   # False negative rate
-    # Precision and Recall
-    prcc = np.mean(sptt.truth == 1)
-    recc = np.sum(sptt.truth == 1) * 1. / fnn
-    # F1 score
-    f1 = (prcc * recc * 1.) / (prcc + recc)
-    # Add to list
-    f1_score2.append(f1)
-
-# Pack togethe rinto data frame
+# Pack together into data frame
 
 res = pd.DataFrame({'N': idx, 'F_score' : f1_score})
-res2 = pd.DataFrame({'N': idxx, 'F_score2' : f1_score2})
 def calc_fscore(b):
-    # Predicted positives and negatives
-    ppos = X_test.index[b == 1.] #//
-    pneg = X_test.index[b == 0.]
-    # True positives
-    tp = np.sum(ppos.isin(d2['Factor / Gene Name']))
-    # False positives
-    fp = np.sum(pneg.isin(d2['Factor / Gene Name']))
-    # False negatives
-    fn = np.sum(~d2['Factor / Gene Name'].isin(ppos))
-    # Precision and Recall
-    prc = tp * 1. / (tp + fp)
-    rec = tp * 1. / (tp + fn)
-    # F1 score
-    f1 = (prc * rec * 1.) / (prc + rec)
-    return f1
-
-def calc_fscore2(b):
     # Predicted positives and negatives
     ppos = X_test.index[b == 1.] #//
     pneg = X_test.index[b == 0.]
@@ -115,16 +80,11 @@ def calc_fscore2(b):
 
 res_univariate = pd.DataFrame({'N' : np.log10(X_test.sum(axis = 0)), 'F_score' : X_test.apply(calc_fscore)})
 res_univariate = res_univariate.dropna()
-res_univariate2 = pd.DataFrame({'N' : np.log10(X_test.sum(axis = 0)), 'F_score' : X_test.apply(calc_fscore2)})
-res_univariate2 = res_univariate2.dropna()
 
 # Plot results
 plt.plot(res_univariate.N, res_univariate.F_score, 'go')
-plt.plot(res_univariate2.N, res_univariate2.F_score, 'ko')
 plt.plot(res.N, res.F_score, 'bo')
 plt.plot(res.N, res.F_score, 'r--')
-plt.plot(res2.N, res2.F_score2, 'bo')
-plt.plot(res2.N, res2.F_score2, 'y--')
 plt.xlabel('N [log10]')
 plt.ylabel('F1 score')
 plt.show()
