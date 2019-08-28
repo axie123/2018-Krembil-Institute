cc6b35680571e91a9d9415335307dfeeb2b73445
gottagitgoing <ruo.cxie@gmail.com>
2019-08-28 12:12:53 -0400

    Update Ridge.py

--- Linear Regression Models/Ridge.py
+++ Linear Regression Models/Ridge.py
@@ -9,8 +9,8 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
 # Imports the main processed file and the validated interactions file. Trims them as well.
-ml_file = pd.DataFrame(pd.read_csv('C:\Users\Andy Xie\Documents\Work\Research\Software\Multi-Factor Analysis\ml_ready.txt', sep = '\t', low_memory = False))
-d2 = pd.DataFrame(pd.read_csv('C:\Users\Andy Xie\Documents\Work\Research\Databases\MI'+'\\'+'training_validated_interactions.txt', sep = '\t', low_memory = False))
+ml_file = pd.DataFrame(pd.read_csv('C:\Users\ml_ready.txt', sep = '\t', low_memory = False))
+d2 = pd.DataFrame(pd.read_csv('C:\Users'+'\\'+'training_validated_interactions.txt', sep = '\t', low_memory = False))
 del(ml_file['Unnamed: 0'])
 d2['Factor / Gene Name'] = d2['TF'] + ' / ' + d2['Target']
 ml_file = ml_file.set_index('Factor / Gene Name')
